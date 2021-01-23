9d7b958893183232eba9ad27fd6716c1cdd6f6a8
awsame303 <tpranav2009@gmail.com>
2021-01-23 09:43:16 -0500

    CheckUser

--- src/libraryManagementSystem/Main.java
+++ src/libraryManagementSystem/Main.java
@@ -1,5 +1,27 @@
 package libraryManagementSystem;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+	public static void main(String[] args) {
+		String person = User();
+		if (person == "Librarian") {
+			System.out.println("You are a Librarian");
+		} else if (person == "User") {
+			System.out.println("You are a User");
+		} else {
+			System.out.println("Error, please try again");
+		}
+
+	}
+
+	public static String User() {
+		String person = "";
+		System.out.println("Enter if Librarian or User");
+		Scanner in = new Scanner(System.in);
+		person = in.next();
+		in.close();
+		return person;
+
+	}
 }
