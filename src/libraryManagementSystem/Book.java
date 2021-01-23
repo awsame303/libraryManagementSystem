e3651091739d2d97df726a4768dfc82862fe5360
awsame303 <tpranav2009@gmail.com>
2021-01-23 18:55:25 -0500

    Class Things

--- src/libraryManagementSystem/Book.java
+++ src/libraryManagementSystem/Book.java
@@ -1,15 +1,17 @@
 package libraryManagementSystem;
+
 import java.util.*;
+
 public class Book {
 	String name;
 	boolean avail = true;
-	
+
 	public void setBookName(String bookName) {
-		name = bookName;	
+		name = bookName;
 	}
+
 	public String getBookName() {
 		return name;
 	}
-	
-	
+
 }
--- src/libraryManagementSystem/Main.java
+++ src/libraryManagementSystem/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
 	List<Book> books = new ArrayList<Book>();
-	
+
 	User user = new User();
 
 	public void process() {
@@ -26,7 +26,7 @@ public class Main {
 
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Main runner = new Main();
 		runner.process();
@@ -44,9 +44,10 @@ public class Main {
 	public void librarianMethod() {
 		int doing = 0;
 		Scanner in = new Scanner(System.in);
-		System.out.println("What would you like to do: 1, Add New Books: 2, Update availability of a book: 3, Add Count to Book: 4, Track Members: 5, Track Transitions of Renewals: 6, Or Show All Books: 7, or Exit: 8");
+		System.out.println(
+				"What would you like to do: 1, Add New Books: 2, Update availability of a book: 3, Add Count to Book: 4, Track Members: 5, Track Transitions of Renewals: 6, Or Show All Books: 7, or Exit: 8");
 		doing = in.nextInt();
-		switch(doing) {
+		switch (doing) {
 		case 1:
 			System.out.println("Enter Book Name: ");
 			Book tempbook = new Book();
@@ -56,32 +57,29 @@ public class Main {
 			books.add(tempbook);
 			break;
 		case 2:
-			
+
 		case 3:
-			
+
 		case 4:
-		
+
 		case 5:
-			
+
 		case 6:
-			
+
 		case 7:
-			for(int i = 0; i < books.size(); i++) {
+			for (int i = 0; i < books.size(); i++) {
 				System.out.println(books.get(i).getBookName());
 			}
-		
+
 		case 8:
 			break;
 		default:
 			System.out.println("Error, make sure caps are right and words are spelled correctly");
 			librarianMethod();
 		}
-		}
-	
-	
-
+	}
 
 	public void userMethod() {
-		
+
 	}
 }
