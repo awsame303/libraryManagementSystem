5395c42e8c96b71ae082d46100de1232af75e622
awsame303 <tpranav2009@gmail.com>
2021-01-23 12:11:39 -0500

    Added different class

--- src/libraryManagementSystem/Librarian.java
+++ src/libraryManagementSystem/Librarian.java
@@ -1,7 +1,10 @@
 package libraryManagementSystem;
-
+import java.util.*;
 public class Librarian {
-	public int addNewBooks() {
-		
+	List<String> books = new ArrayList<String>();
+	public void addNewBooks() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the name of the book you would like to enter?");
+		books.add(in.nextLine());	
 	}
 }
